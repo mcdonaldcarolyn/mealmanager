ace4c322a1113694d7e8ec53e913f95646733972
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-12 15:48:12 -0400

    nested ingredient routes under meals

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :meals
-resources :ingredients
+resources :meals do 
+  resources :ingredients
+end
 resources :meals_ingredients
 end
