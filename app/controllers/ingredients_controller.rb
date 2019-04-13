f9eaccd091615db00f3484d8d803ac8f4402781a
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-13 15:02:43 -0400

    add index method to ingredient controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,15 @@
 class IngredientsController < ApplicationController
+
+  def index 
+    if params[:meals_id]
+      @meal = Meal.find(params[:meal_id])
+      @ingredients = @meal.ingredients
+      @meal_ingredient = MealIngredient.new
+      render :show
+    else
+      @ingredients = Ingredient.all
+    end
+  end
+
+  
 end
