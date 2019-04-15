bacfb6f86c0144fd2874580eba123c80146a8508
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 15:42:43 -0400

    fix type on drop don units

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -9,6 +9,6 @@ class Meal < ApplicationRecord
   end
 
   def self.glutenfree
-    where("id not in (select meal_id from meals_ingredients mi inner join ingredients i on mi.ingredient_id = i.id where i.name in ('pasta', 'bread', 'ziti', 'flour', 'farro'))")
+    where("id not in (select meal_id from meals_ingredients mi inner join ingredients i on mi.ingredient_id = i.id where i.name in ('pasta', 'bread', 'ziti', 'flour', 'farro', 'dough'))")
   end
 end
