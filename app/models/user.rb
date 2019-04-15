3818cf169c2bad14ff17ab01d280fb21e5582c3b
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-14 20:49:53 -0400

    add omni auth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+   def self.from_omniauth(auth)
+
+    self.where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email= auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+          data = access_token.info
+          user = User.where(email: data['email']).first
+   user
+        end 
+
+ end
+
