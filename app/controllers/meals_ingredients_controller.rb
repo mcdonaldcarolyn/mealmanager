e8e0f8d672cb2b3fc790bdd42b6e5e80d1b1d834
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 13:20:20 -0400

    add params_check method to mi model

--- app/controllers/meals_ingredients_controller.rb
+++ app/controllers/meals_ingredients_controller.rb
@@ -2,5 +2,6 @@ class MealsIngredientsController < ApplicationController
 
 
   def create
+    if meals_ingredient_params[:ingredient_id].length > 0
   end
 end
--- app/models/meals_ingredient.rb
+++ app/models/meals_ingredient.rb
@@ -2,4 +2,15 @@ class MealsIngredient < ApplicationRecord
   belongs_to :meals
   belongs_to :ingredient
   accepts_nested_attributes_for :ingredient
+
+  def self.params_check 
+  
+     meals_ingredient_params[:ingredient_id].length > 0
+      meal_ingredient = MealsIngredient.create({
+        :meal_id => meals_ingredient_params[:meal_id],
+        :ingredient_id => meals_ingredient_params[:ingredient_id],
+        :amount => meals_ingredient_params[:amount]
+        :unit => meals_ingredients_params[:unit]
+      })
+  end
 end
