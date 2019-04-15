0ec7992e408db8e9f7dd7c30d2ec6b5f28451b33
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 13:38:52 -0400

    fix mi/create and model methods

--- app/controllers/meals_ingredients_controller.rb
+++ app/controllers/meals_ingredients_controller.rb
@@ -3,11 +3,17 @@ class MealsIngredientsController < ApplicationController
 
   def create
     if meals_ingredient_params[:ingredient_id].length > 0
-      MealsIngredients.params_check
+      MealsIngredients.params_check(meals_ingredient_params)
       redirect_to meal_ingredients_path(meal_ingredient.meal)
     else
-      Meals.Ingredient.params_make
-      redirect_to meal_ingredients_path(meal_ingredient.meal)
-
+      MealsIngredient.params_make(meals_ingredient_params)
+      redirect_to meals_ingredients_path(meal_ingredient.meal)
+    end
   end
+
+private 
+def meals_ingredient_params
+  params.require(:meals_ingredient).permit(:amount, :unit, :meal_id, :ingredient_id, ingredient:[:name])
+end
+
 end
--- app/models/meals_ingredient.rb
+++ app/models/meals_ingredient.rb
@@ -3,25 +3,27 @@ class MealsIngredient < ApplicationRecord
   belongs_to :ingredient
   accepts_nested_attributes_for :ingredient
 
-  def self.params_check 
+  def self.params_check (meals_ingredient_params)
   
      meals_ingredient_params[:ingredient_id].length > 0
       meal_ingredient = MealsIngredient.create({
         :meal_id => meals_ingredient_params[:meal_id],
         :ingredient_id => meals_ingredient_params[:ingredient_id],
-        :amount => meals_ingredient_params[:amount]
-        :unit => meals_ingredients_params[:unit]
+        :amount => meals_ingredient_params[:amount],
+        :unit => meals_ingredient_params[:unit]
       })
   end
 
-  def self.params_make
+  def self.params_make(meals_ingredient_params)
     ingredient = Ingredient.find_or_create_by({
       :name => meals_ingredient_params[:ingredient][:name]
     })
     meal_ingredient = MealsIngredient.create({
       :meal_id => meals_ingredient_params[:meal_id],
       :ingredient_id => ingredient.id,
-      :amount => meals_ingredient_params[:amount]
-      :unit => meals_ingredients_params[:unit]
+      :amount => meals_ingredient_params[:amount],
+      :unit => meals_ingredient_params[:unit]
+    })
   end
+
 end
