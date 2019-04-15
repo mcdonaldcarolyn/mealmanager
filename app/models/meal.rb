e0a0e1c0ab78b65ab6f760fe00b45d116f8233f5
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 15:37:58 -0400

    add SQl methods to meals model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,13 @@ class Meal < ApplicationRecord
   has_many :meals_ingredients
   has_many :ingredients, through:  :meals_ingredients
   accepts_nested_attributes_for :ingredients
+
+
+  def self.vegetarian
+    where("id not in (select meal_id from meals_ingredients mi inner join ingredients i on mi.ingredient_id = i.id where i.name in ('chicken', 'steak', 'beef', 'ground beef'))")
+  end
+
+  def self.glutenfree
+    where("id not in (select meal_id from meals_ingredients mi inner join ingredients i on mi.ingredient_id = i.id where i.name in ('pasta', 'bread', 'ziti', 'flour', 'farro'))")
+  end
 end
