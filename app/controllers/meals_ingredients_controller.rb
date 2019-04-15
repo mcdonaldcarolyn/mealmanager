951c7cbf56740239567876e08243e22f95c653b0
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 13:23:19 -0400

    create mi/create

--- app/controllers/meals_ingredients_controller.rb
+++ app/controllers/meals_ingredients_controller.rb
@@ -3,5 +3,11 @@ class MealsIngredientsController < ApplicationController
 
   def create
     if meals_ingredient_params[:ingredient_id].length > 0
+      MealsIngredients.params_check
+      redirect_to meal_ingredients_path(meal_ingredient.meal)
+    else
+      Meals.Ingredient.params_make
+      redirect_to meal_ingredients_path(meal_ingredient.meal)
+
   end
 end
