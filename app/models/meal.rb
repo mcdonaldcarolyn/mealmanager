2474d1adc014edae3e0979f0e38ce10467cbd11b
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 21:31:00 -0400

    add validation to meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,7 @@ class Meal < ApplicationRecord
   has_many :meals_ingredients
   has_many :ingredients, through:  :meals_ingredients
   accepts_nested_attributes_for :ingredients
-  
+  validates :title,  presence: true, uniqueness: true
 
   def self.vegetarian
     where("id not in (select meal_id from meals_ingredients mi inner join ingredients i on mi.ingredient_id = i.id where i.name in ('chicken', 'steak', 'beef', 'ground beef'))")
