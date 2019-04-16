163bd09d14199d938c0af9fa0242abec5a3fcf08
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-15 21:34:04 -0400

    fix validation to ingredient and mealsingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
     has_many :meals_ingredients
     has_many :meals, through: :meals_ingredients
-    validates :title,  presence: true, uniqueness: true
+    validates :name,  presence: true, uniqueness: true
 end
