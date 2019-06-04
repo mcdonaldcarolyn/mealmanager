8bd879f9e65a04e082280ddf29706738c584b5d2
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-04 13:48:36 -0400

    start js code for meal edit

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -1,6 +1,10 @@
+$(document).ready(()=>{
+    console.log("hello")
+$('a#editMeal').on('click', (e)=>{
+    e.preventDefault();
+    
+})
 
 
-// fetch('/meals/index')
-
-console.log('hello');
+})
 
