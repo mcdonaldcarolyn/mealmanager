b5fc349bd75a7e1e560b604ee65909d329ad195b
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-04 13:47:09 -0400

    started ingredient javascript

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,22 +1,16 @@
+$(document).ready(()=>{
+    console.log("hello")
+})
 
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     // change this to the route in your controller to return json
+//     fetch('/ingredients/getsomething')
+//         .then(resp => resp.json)
+//         .then(ingredients => {
+//             // confirm on index ingredients that this is in devtools console
+//             console.log(ingredients);
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    // change this to the route in your controller to return json
-    fetch('/ingredients/getsomething')
-        .then(resp => resp.json)
-        .then(ingredients => {
-            // confirm on index ingredients that this is in devtools console
-            console.log(ingredients);
-
-            // select a div on the page <div id="ingredients"></div>
-            const ingredientsDiv = document.querySelector('#ingredients');
-
-            // inserting stuff into the dom
-            ingredients.forEach(ingredient => {
-                // insert new element into ingredientsDiv
-                const newDiv = document.createElement('div');
-                ingredientsDiv.append(newDiv);
-            });
-        });  
-});
-
+//             // select a div on the page <div id="ingredients"></div>
+    
+//         })
+//     })
