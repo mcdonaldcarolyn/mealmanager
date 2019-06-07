3b29929e56153e87d8a71d7d52aa4d843bda52a9
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-07 10:23:53 -0400

    add js and html for inserting data

--- app/assets/javascripts/meals_ingredients.js
+++ app/assets/javascripts/meals_ingredients.js
@@ -1,14 +1,37 @@
 $(document).ready(()=>{
-    console.log("hello")
+    console.log("hello");
    
-})
-    let meals = Meal.all
-    console.log(meals)
+    $('a#editMeal').click(function (evt) {
+        evt.preventDefault();
+        const id = evt.target.getAttribute('data-meal-id');
+        $.get('/meals_json/' + id, function(ingredients) {
+            console.log(ingredients);
+            let htmlStr = '<ul>';
 
-   $('a#editMeal').on('click', (e)=>{
-     e.preventDefault();
+            for (let i = 0; i < ingredients.length; i++) {
+                const ingredient = ingredients[i];
+                htmlStr = htmlStr + '<li>' + ingredient.name + '</li>';
+            }
+
+            htmlStr += '</ul>';
+
+            $('#editForm-' + id).html(htmlStr);
+
+            
+
+         });
+    });
+
+
+ 
+})
     
 
+
+//    $('/meals_json/:id').on('click', (e)=>{
+//      e.preventDefault();
+//    })
+   
   //     $( '@meals_ingredients', {
 //             success: function (data) {
 //                 console.log(data);
