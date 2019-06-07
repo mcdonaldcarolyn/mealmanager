b882712e15c4cfb935a771f6e21daade326686d4
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-07 10:25:33 -0400

    ingredients ojbject controller for route

--- app/controllers/meals_json_controller.rb
+++ app/controllers/meals_json_controller.rb
@@ -1,2 +1,7 @@
 class MealsJsonController < ApplicationController
+    def show 
+        @meal = Meal.find(params[:id])
+        @ingredients = @meal.ingredients
+        render json: @ingredients
+    end
 end
