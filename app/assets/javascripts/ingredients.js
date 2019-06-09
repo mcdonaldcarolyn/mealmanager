01afdb355c08cc94c6d2a4ed1b024884b00f5783
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-09 17:18:13 -0400

    add post and create ingredeint obj

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -17,7 +17,29 @@ $(document).ready(() =>{
 
             $('#editForm-' + id).html(htmlStr);
 
-            
+            $('#hiddenAdd-' + id).show();
+            $('#hiddenAdd-' + id).click(function (evt) {
+                evt.preventDefault();
+                $('#formAdd-' + id).show();
+
+                $(`#formAdd-${id} > form`).submit(function (evt) {
+                    evt.preventDefault();
+                    createIngredientObj(evt, id)
+                });
+            });
+            function createIngredientObj(evt, id){
+                evt.preventDefault();
+
+                let value = $(`#formAdd-${id} > form`).serializeArray()
+                
+                console.log('value is ');
+                console.log(value);
+
+                $.post('/meals_ingredients', value, function(resp_obj){
+                   console.log("got to here");
+                   console.log(resp_obj);
+                })
+            }
 
         });
     });
