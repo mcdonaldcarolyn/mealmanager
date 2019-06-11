4124aead7bb242afd6cb55b0c94d3a6904a153c2
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-11 19:37:34 -0400

    add id in function

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -17,38 +17,45 @@ $(document).ready(() =>{
 
             $('#editForm-' + id).html(htmlStr);
 
-            $('#hiddenAdd-' + id).show();
-            $('#hiddenAdd-' + id).click(function (evt) {
-                evt.preventDefault();
-                $('#formAdd-' + id).show();
-
-                $(`#formAdd-${id} > form`).submit(function (evt) {
-                    evt.preventDefault();
-                    createIngredientObj(evt, id)
-                });
-            });
-            function createIngredientObj(evt, id){
-                evt.preventDefault();
-
-                let value = $(`#formAdd-${id} > form`).serializeArray()
-                
-                console.log('value is ');
-                console.log(value);
-
-                $.post('/meals_ingredients', value, function(resp_obj) {
-
-                        const ingredient = new Ingredient(resp_obj);
-                        ingredient.showConfirmationMessage();
-
-                        // this is just to add the ingredient to the list
-                        const nameValue = value.filter(item => item.name === 'ingredient[name]');
-                        $(`#editForm-${id} ul`).append(`<li>${nameValue[0].value}</li>`);
-                        $('#formAdd-' + id).hide();   
-                    }
-                );
-            }
+            
+            showForm()
         });
     });
+})
+
+function showForm(id){
+    $('#hiddenAdd-' + id).show();
+    $('#hiddenAdd-' + id).click(function (evt) {
+        evt.preventDefault();
+        $('#formAdd-' + id).show();
+
+        $(`#formAdd-${id} > form`).submit(function (evt) {
+            evt.preventDefault();
+            createIngredientObj(evt, id)
+        });
+    });
+}
+    function createIngredientObj(evt, id){
+        evt.preventDefault();
+
+        let value = $(`#formAdd-${id} > form`).serializeArray()
+        
+        console.log('value is ');
+        console.log(value);
+
+        $.post('/meals_ingredients', value, function(resp_obj) {
+
+            const ingredient = new Ingredient(resp_obj);
+            ingredient.showConfirmationMessage();
+
+            // this is just to add the ingredient to the list
+            const nameValue = value.filter(item => item.name === 'ingredient[name]');
+            $(`#editForm-${id} ul`).append(`<li>${nameValue[0].value}</li>`);
+            $('#formAdd-' + id).hide();   
+    
+        });
+    }   
+
 
     class Ingredient {
         constructor(data) {
@@ -60,6 +67,6 @@ $(document).ready(() =>{
             window.alert('New ingredient added: ' + this.name);
         }
     }  
-});
+
     
 
