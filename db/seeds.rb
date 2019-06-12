df9ccd22776f8a4b5e5de8c2122c01d3f01c7451
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-12 11:15:43 -0400

    remove button for ingredients working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Meal.create (name: 'Pizza' )
+Meal.create (name: 'Lasagna')
