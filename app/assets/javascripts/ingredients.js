d3bdfa8631294c1d3fdd3a17cecb21d4c348e961
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-06-12 05:26:58 -0400

    WIP create show page for ingredient

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,27 +1,36 @@
-$(document).ready(() =>{
+
+$(document).ready(() => {
+    addIngredientListClickListener();
+    addIngredientShowClickListener();
+});
+
+function addIngredientListClickListener() {
     console.log("ingredients.js");
-   
-    $('a#editMeal').click(function (evt) {
-        evt.preventDefault();
-        const id = evt.target.getAttribute('data-meal-id');
-        $.get('/meals/' + id + '/ingredients.json', function(ingredients) {
-            console.log(ingredients);
-            let htmlStr = '<ul>';
+    $('a#editMeal').click(editMealClick);
+}
 
-            for (let i = 0; i < ingredients.length; i++) {
-                const ingredient = ingredients[i];
-                htmlStr = htmlStr + '<li>' + ingredient.name + '</li>';
-            }
-
-            htmlStr += '</ul>';
-
-            $('#editForm-' + id).html(htmlStr);
-
-            
-            showForm()
-        });
+function editMealClick(evt) {
+    evt.preventDefault();
+    const id = evt.target.getAttribute('data-meal-id');
+    $.get('/meals/' + id + '/ingredients.json', function (ingredients) {
+        console.log(ingredients);
+        showIngredients(id, ingredients);
+        showForm(id);
     });
-})
+}
+
+function showIngredients(id, ingredients) {
+    let htmlStr = '<ul>';
+
+    for (let i = 0; i < ingredients.length; i++) {
+        const ingredient = ingredients[i];
+        htmlStr = htmlStr + '<li>' + ingredient.name + '</li>';
+    }
+
+    htmlStr += '</ul>';
+
+    $('#editForm-' + id).html(htmlStr);
+}
 
 function showForm(id){
     $('#hiddenAdd-' + id).show();
@@ -35,38 +44,55 @@ function showForm(id){
         });
     });
 }
-    function createIngredientObj(evt, id){
-        evt.preventDefault();
 
-        let value = $(`#formAdd-${id} > form`).serializeArray()
+function createIngredientObj(evt, id){
+    evt.preventDefault();
+
+    let value = $(`#formAdd-${id} > form`).serializeArray()
+    
+    console.log('value is ');
+    console.log(value);
+
+    $.post('/meals_ingredients', value, function(resp_obj) {
+
+        const ingredient = new Ingredient(resp_obj);
+        ingredient.showConfirmationMessage();
+
+        // this is just to add the ingredient to the list
+        const nameValue = value.filter(item => item.name === 'ingredient[name]');
+        $(`#editForm-${id} ul`).append(`<li>${nameValue[0].value}</li>`);
+        $('#formAdd-' + id).hide();   
+
+    });
+}   
+
+
+class Ingredient {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+    }
+
+    showConfirmationMessage() {
+        window.alert('New ingredient added: ' + this.name);
+    }
+}  
+  
+function addIngredientShowClickListener() {
+    $('a#ingredientName').click(ingredientNameClick);
+
+} 
+function ingredientNameClick(evt){
+    evt.preventDefault();
+    const ingred = evt.target.getAttribute(ingredient_id);
+    $.get('/ingredients/${id}.json', function (ingredients) {
+        showMeals(id);
         
-        console.log('value is ');
-        console.log(value);
-
-        $.post('/meals_ingredients', value, function(resp_obj) {
-
-            const ingredient = new Ingredient(resp_obj);
-            ingredient.showConfirmationMessage();
-
-            // this is just to add the ingredient to the list
-            const nameValue = value.filter(item => item.name === 'ingredient[name]');
-            $(`#editForm-${id} ul`).append(`<li>${nameValue[0].value}</li>`);
-            $('#formAdd-' + id).hide();   
-    
-        });
-    }   
-
-
-    class Ingredient {
-        constructor(data) {
-            this.id = data.id;
-            this.name = data.name;
-        }
-
-        showConfirmationMessage() {
-            window.alert('New ingredient added: ' + this.name);
-        }
-    }  
-
-    
-
+    });
+}
+function showMeals(id, ){
+    let showstr = '<ul>';
+    for (let i = 0; i < meal.length; i++) {
+        const ingredient = meals[i];
+        htmlStr = htmlStr + '<li>' + ingredient.name + '</li>';
+}
